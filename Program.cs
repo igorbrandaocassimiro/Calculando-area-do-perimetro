0bcf1c7e61ac30d06309928e4ff41952ca1f9087
Igor Brandão <igorbrandaocassimirodaflon@gmail.com>
2025-08-14 14:38:38 -0300

    Primeiro commit do programa

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] arge)
+    {
+        double raio = 5.0;  
+        double pi = 3.14159; 
+
+        double area = pi * raio * raio;  
+        double perimetro = 2 * pi * raio; 
+
+        Console.WriteLine("Raio: " + raio.ToString("F2"));  
+        Console.WriteLine("Área do círculo: " + area.ToString("F2"));  
+        Console.WriteLine("Perímetro do círculo: " + perimetro.ToString("F2")); 
+    }
+}
